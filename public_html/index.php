4e95ed02f4deccfb361ecf0def537b8245e37c8e
Michael Romero <m.romero1989@gmail.com>
2017-12-04 07:54:56 -0700

    Made a brand new framework for my pwp.

---
+++ public_html/index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!--jQuery, Popper.js, Bootstrap JS, Bootstrap CSS-->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
+
+		<!--CSS file link-->
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/jquery-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<link href="https://fonts.googleapis.com/css?family=Cutive+Mono" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+		<!--social media icons-->
+		<script src="https://use.fontawesome.com/f75c8f006c.js"></script>
+
+		<title>Build & Work</title>
+
+	</head>
+
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-white .navbar-default p-5">
+			<a class="navbar-brand font-weight-bold" href="#">Build & Work</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarText">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link text-dark" href="#about">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-dark" href="#">Work</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link text-dark" href="#">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		<div class="container p-2">
+			<div class="row">
+				<div class="col-sm-12">
+					<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+							<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+							<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+						</ol>
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img class="d-block w-100" src="assets/images/17.jpg" alt="First slide">
+								<div class="carousel-caption d-none d-md-block">
+									<h3 class="font-weight-bold">Design</h3>
+									<p class="image-text font-weight-bold">We believe that design is art. Building beautiful websites and apps is a passion that we love. </p>
+								</div>
+							</div>
+							<div class="carousel-item">
+								<img class="d-block w-100" src="assets/images/18.jpg" alt="Second slide">
+								<div class="carousel-caption d-none d-md-block">
+									<h3 class="font-weight-bold">Mesmerizing</h3>
+									<p class="image-text font-weight-bold">We like images that pop and we believe that design has the power to make you feel. We want our projects to reflect the beauty all our around us. We want people to look at something truly amazing.</p>
+								</div>
+							</div>
+							<div class="carousel-item">
+								<img class="d-block w-100" src="assets/images/19.jpg" alt="Third slide">
+								<div class="carousel-caption d-none d-md-block">
+									<h3 class="font-weight-bold">Exciting</h3>
+									<p class="image-text font-weight-bold">Creating things is exciting and we want to create for you. Whatever idea you have, we want to do. Contact us for more information. We would love to talk to you!</p>
+								</div>
+							</div>
+						</div>
+						<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 about text-center p-5" id="about">
+					About
+				</div>
+				<div class="col-sm-12 p-5">
+					<p class="text-justify font-italic">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 about text-center p-5" id="about">
+					Work
+				</div>
+				<div class="card-group p-5">
+					<div class="card">
+						<img class="card-img-top" src="assets/images/25.jpg" alt="Card image cap">
+						<div class="card-body">
+							<h4 class="card-title">Demo 1</h4>
+							<p class="card-text">Exceeding reaction chamber thermal limit. We have begun power-supply calibration. Force fields have been established on all turbo lifts and crawlways. Computer, run a level-two diagnostic on warp-drive systems. Antimatter containment positive. Warp drive within normal parameters. I read an ion trail characteristic of a freighter escape pod. The bomb had a molecular-decay detonator. Detecting some unusual fluctuations in subspace frequencies.</p>
+							<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+						</div>
+					</div>
+					<div class="card">
+						<img class="card-img-top" src="assets/images/26.jpg" alt="Card image cap">
+						<div class="card-body">
+							<h4 class="card-title">Demo 2</h4>
+							<p class="card-text"><p>
+								Sensors indicate human life forms 30 meters below the planet's surface. Stellar flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar 006, warp seven. There's no evidence of an advanced communication network. Total guidance system failure, with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent. We have covered the area in a spherical pattern which a ship without warp drive could cross in the given time.</p>
+							<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+						</div>
+					</div>
+					<div class="card">
+						<img class="card-img-top" src="assets/images/25.jpg" alt="Card image cap">
+						<div class="card-body">
+							<h4 class="card-title">KindHub</h4>
+							<p class="card-text">Communication is not possible. The shuttle has no power. Using the gravitational pull of a star to slingshot back in time? We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of geological activity in all five planetary systems. Assemble a team. Look at records of the Drema quadrant. Would these scans detect artificial transmissions as well as natural signals?</p>
+							<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 about text-center p-5" id="about">
+					Contact
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<form id="contact" action="php/mailer.php" method="post" novalidate class="mx-auto p-5">
+					<fieldset>
+						<input placeholder="name......." type="text" id="name" name="name" tabindex="1" required autofocus>
+					</fieldset>
+					<fieldset>
+						<input placeholder="email......" type="email" id="email" name="email" tabindex="2" required>
+					</fieldset>
+					<fieldset>
+						<input placeholder="phone......" type="tel" id="phone" name="phone" tabindex="3" required>
+					</fieldset>
+					<fieldset>
+						<input placeholder="message......" type="tel" id="message" name="message" tabindex="5" required>
+					</fieldset>
+					<!--google recaptcha-->
+					<div class="g-recaptcha mt-3" data-sitekey="6LeslToUAAAAAG1s1ktnY_cC3pmDcw_oHhxZAhyK"></div>
+					<fieldset>
+						<button class="btn btn-success slide__link mt-3" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+						<button class="btn btn-warning slide__link mt-3" type="reset"><i class="fa fa-ban"></i> Reset</button>
+<!--						<a class="nav-link text-dark justify-content" href="">submit</a>-->
+<!--						<a class="nav-link text-dark mx-auto" href="">reset</a>-->
+					</fieldset>
+				</form>
+			</div>
+		</div>
+
+
+		<footer>
+			<div class="container p-5">
+				<div class="row">
+					<address>
+						Build & Work
+						<br>
+						20 1st Plaza Center NW #200
+						<br>
+						Albuquerque, NM 87102
+						<br>
+						P: (123) 456-7890
+						<br>
+						<a href="mailto:bldwrk@icloud.com">bldwrk@icloud.com</a>
+					</address>
+					<a class="btn btn-social-icon btn-twitter ml-auto">
+						<span class="fa fa-twitter"></span>
+					</a>
+					<a class="btn btn-social-icon btn-facebook">
+						<span class="fa fa-facebook"></span>
+					</a>
+					<a class="btn btn-social-icon btn-linkedin">
+						<span class="fa fa-linkedin"></span>
+					</a>
+					<a class="btn btn-social-icon btn-instagram">
+						<span class="fa fa-instagram mr-auto"></span>
+					</a>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
