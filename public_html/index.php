586ac1b88a69c173d752e1922e1a7badbc6bf853
Michael Romero <m.romero1989@gmail.com>
2017-12-07 15:06:12 -0700

    changed color of nav links

--- public_html/index.php
+++ public_html/index.php
@@ -36,25 +36,27 @@
 	</head>
 
 	<body>
-		<nav class="navbar navbar-expand-lg navbar-light bg-white .navbar-default p-5">
-			<a class="navbar-brand font-weight-bold" href="#">Build & Work</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarText">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item">
-						<a class="nav-link text-dark" href="#about">About</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link text-dark" href="#work">Work</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link text-dark" href="#contact1">Contact</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
+		<div class="container py-5">
+			<nav class="navbar navbar-expand-lg navbar-light bg-white .navbar-default">
+				<a class="navbar-brand font-weight-bold" href="#">Build & Work</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarText">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link text-primary" href="#about">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-primary" href="#work">Work</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link text-primary" href="#contact1">Contact</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
 
 		<div class="container p-3 mb-3">
 			<div class="row">
@@ -69,22 +71,22 @@
 							<div class="carousel-item active">
 								<img class="d-block w-100" src="assets/images/17.jpg" alt="First slide">
 								<div class="carousel-caption d-none d-md-block">
-									<h3 class="font-weight-bold">Design</h3>
-									<p class="image-text font-weight-bold">We believe that design is art. Building beautiful websites and apps is what we love to do. </p>
+									<h3 class="font-weight-bold">Minimal Design</h3>
+									<p class="image-text bg-white text-dark">I believe that good design is done simply. I strive to make things aesthetically pleasing. If you catch the users eye they will love it. </p>
 								</div>
 							</div>
 							<div class="carousel-item">
 								<img class="d-block w-100" src="assets/images/18.jpg" alt="Second slide">
 								<div class="carousel-caption d-none d-md-block">
-									<h3 class="font-weight-bold">Mesmerizing</h3>
-									<p class="image-text font-weight-bold">We like images that pop and we believe that design has the power to make you feel. We want our projects to reflect the beauty all our around us. We want people to look at something truly amazing.</p>
+									<h3 class="font-weight-bold">UX</h3>
+									<p class="image-text bg-white text-dark">I like images that pop and I believe that design has the power to make you feel. I want our projects to reflect the beauty all our around us. I want people to look at something truly amazing. But the most important thing I want is a good user experience. Something simple that works while also providing functionality.</p>
 								</div>
 							</div>
 							<div class="carousel-item">
 								<img class="d-block w-100" src="assets/images/19.jpg" alt="Third slide">
 								<div class="carousel-caption d-none d-md-block">
-									<h3 class="font-weight-bold">Exciting</h3>
-									<p class="image-text font-weight-bold">Creating beautiful websites is exciting and we want to create for you. Whatever idea you have, we want to do. Contact us for more information. We would love to talk to you!</p>
+									<h3 class="font-weight-bold">Custom</h3>
+									<p class="image-text bg-white text-dark">Who likes a site that looks like every other site? I don't. I want to create something custom for you whether it be a website or application. It is new, engaging and custom. It is different and in a good way. Stick out from the crowd.</p>
 								</div>
 							</div>
 						</div>
@@ -103,46 +105,43 @@
 
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-12 about text-center p-5" id="about">
+				<div class="col-sm-12 about text-center p-5 font-weight-bold" id="about">
 					About
 				</div>
 				<div class="col-sm-12 p-5">
-					<p class="text-justify font-italic">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+					<p class="text-justify">I specialize in web design, UI/UX design, graphic design, music production and film making. I am a creative at heart and have a history in music production, photography, writing and film. I use PHP, Javascript, HTML/CSS, Bootstrap and Angular in creating my websites. I want to create experiences and to tell a story. What story do you have? What would you like the world to see? I can help you on the path to creating something beautiful, something that's truly amazing. </p>
 				</div>
 			</div>
 		</div>
 
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-12 about text-center p-5" id="work">
+				<div class="col-sm-12 about text-center p-5 font-weight-bold" id="work">
 					Work
 				</div>
 				<div class="card-group p-5">
 					<div class="card">
-						<img class="card-img-top" src="assets/images/25.jpg" alt="Card image cap">
+						<img class="card-img-top" src="assets/images/screen1.png" alt="Card image cap">
 						<div class="card-body">
-							<h4 class="card-title">Demo 1</h4>
-							<p class="card-text">Exceeding reaction chamber thermal limit. We have begun power-supply calibration. Force fields have been established on all turbo lifts and crawlways. Computer, run a level-two diagnostic on warp-drive systems. Antimatter containment positive. Warp drive within normal parameters. I read an ion trail characteristic of a freighter escape pod. The bomb had a molecular-decay detonator. Detecting some unusual fluctuations in subspace frequencies.</p>
+							<h6 class="card-title">Concept No.1</h6>
+							<p class="card-text">Simple site using J-invert scroll. (Under Construction).</p>
 							<a class="nav-link text-primary" href="https://bootcamp-coders.cnm.edu/~mromero308/buildandwork/public_html/" target="_blank">Explore</a>
-							<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 						</div>
 					</div>
 					<div class="card">
-						<img class="card-img-top" src="assets/images/26.jpg" alt="Card image cap">
+						<img class="card-img-top" src="assets/images/screen2.png" alt="Card image cap">
 						<div class="card-body">
-							<h4 class="card-title">Demo 2</h4>
+							<h6 class="card-title">Concept No.2</h6>
 							<p class="card-text"><p>
-								Sensors indicate human life forms 30 meters below the planet's surface. Stellar flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar 006, warp seven. There's no evidence of an advanced communication network. Total guidance system failure, with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent. We have covered the area in a spherical pattern which a ship without warp drive could cross in the given time.</p>
+								Static site using a full page slider. (Under Construction)</p>
 							<a class="nav-link text-primary" href="https://bootcamp-coders.cnm.edu/~mromero308/pwp/public_html/" target="_blank">Explore</a>
-							<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 						</div>
 					</div>
 					<div class="card">
-						<img class="card-img-top" src="assets/images/25.jpg" alt="Card image cap">
+						<img class="card-img-top" src="assets/images/screen3.png" alt="Card image cap">
 						<div class="card-body">
-							<h4 class="card-title">KindHub</h4>
-							<p class="card-text">Communication is not possible. The shuttle has no power. Using the gravitational pull of a star to slingshot back in time? We are going to Starbase Montgomery for Engineering consultations prompted by minor read-out anomalies. Probes have recorded unusual levels of geological activity in all five planetary systems. Assemble a team. Look at records of the Drema quadrant. Would these scans detect artificial transmissions as well as natural signals?</p>
-							<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+							<h6 class="card-title">KindHub App</h6>
+							<p class="card-text">An App to help your neighbor. (under construction)</p>
 						</div>
 					</div>
 				</div>
@@ -152,7 +151,7 @@
 
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-12 about text-center p-5" id="contact1">
+				<div class="col-sm-12 about text-center p-5 font-weight-bold" id="contact1">
 					Contact
 				</div>
 			</div>
@@ -162,7 +161,7 @@
 			<div class="row">
 				<form id="contact" action="php/mailer.php" method="post" novalidate class="mx-auto p-5">
 					<fieldset>
-						<input placeholder="name......." type="text" id="name" name="name" tabindex="1" required autofocus>
+						<input placeholder="name......." type="text" id="name" name="name" tabindex="1" required>
 					</fieldset>
 					<fieldset>
 						<input placeholder="email......" type="email" id="email" name="email" tabindex="2" required>
@@ -171,16 +170,16 @@
 						<input placeholder="phone......" type="tel" id="phone" name="phone" tabindex="3" required>
 					</fieldset>
 					<fieldset>
-						<input placeholder="message......" type="tel" id="message" name="message" tabindex="5" required>
+						<textarea id="message" name="message" required placeholder="message......"></textarea>
 					</fieldset>
 					<!--google recaptcha-->
 					<div class="g-recaptcha mt-3" data-sitekey="6LeslToUAAAAAG1s1ktnY_cC3pmDcw_oHhxZAhyK"></div>
-						<button class="btn btn-success slide__link mt-3" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-						<button class="btn btn-warning slide__link mt-3" type="reset"><i class="fa fa-ban"></i> Reset</button>
+						<button class="btn btn-primary slide__link mt-3 ml-5" type="submit"><i class="fa fa-paper-plane-o"></i> Send</button>
+						<button class="btn btn-dark slide__link mt-3" type="reset"><i class="fa fa-ban"></i> Reset</button>
 				</form>
 			</div>
 			<div class="row">
-				<div class="col-sm-4 offset sm-4">
+				<div class="col-sm-4 offset sm-4 mx-auto">
 					<div id="output-area"></div>
 				</div>
 			</div>
@@ -191,30 +190,31 @@
 			<div class="container p-5">
 				<div class="row">
 					<address>
-						Build & Work
-						<br>
-						20 1st Plaza Center NW #200
-						<br>
-						Albuquerque, NM 87102
-						<br>
-						P: (123) 456-7890
-						<br>
-						<a href="mailto:bldwrk@icloud.com">bldwrk@icloud.com</a>
+						<ul>
+							<li>Build & Work</li>
+							<li>20 1st Plaza Center NW #200</li>
+							<li>Albuquerque, NM 87102</li>
+							<li><a href="mailto:bldwrk@icloud.com">bldwrk@icloud.com</a></li>
+						</ul>
 					</address>
-					<a class="btn btn-social-icon btn-twitter ml-auto" href="https://twitter.com" target="_blank">
-						<span class="fa fa-twitter"></span>
-					</a>
-					<a class="btn btn-social-icon btn-facebook" href="https://www.facebook.com" target="_blank">
-						<span class="fa fa-facebook"></span>
-					</a>
-					<a class="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com" target="_blank">
-						<span class="fa fa-linkedin"></span>
-					</a>
-					<a class="btn btn-social-icon btn-instagram" href="https://www.instagram.com" target="_blank">
-						<span class="fa fa-instagram mr-auto"></span>
+					<div class="ml-auto pr-5">
+						<a class="btn btn-social-icon btn-twitter" href="https://twitter.com" target="_blank">
+							<span class="fa fa-twitter"></span>
+						</a>
+						<a class="btn btn-social-icon btn-facebook" href="https://www.facebook.com" target="_blank">
+							<span class="fa fa-facebook"></span>
+						</a>
+						<a class="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com" target="_blank">
+							<span class="fa fa-linkedin"></span>
+						</a>
+						<a class="btn btn-social-icon btn-instagram" href="https://www.instagram.com/ifthen_else/" target="_blank">
+							<span class="fa fa-instagram"></span>
+						</a>
+					</div>
 					</a>
 				</div>
 			</div>
+
 		</footer>
 
 	</body>
